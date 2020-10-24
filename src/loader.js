f129e38719c304f9ca6b71e43fd434452e0ab57d
SwxtchCode <swxtchcode@protonmail.com>
2020-10-24 10:59:20 -0700

    Update loader.js

--- src/loader.js
+++ src/loader.js
@@ -1,1 +1,1 @@
-(async()=>{eval(await(await fetch("https://raw.githubusercontent.com/SwxtchCode/SwxtchCodesAndroidCheatMenu/main/src/index.js")).text())})();
+(async()=>{eval(await(await fetch('https://raw.githubusercontent.com/SwxtchCode/SwxtchCodesAndroidCheatMenu/main/src/index.js')).text())})();
